4713fdf93e0be295a3a721e83da4067e428b4b97
nadsphere <omgnnh@gmail.com>
2022-08-13 19:25:09 +0700

    test 22

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,8 +4,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-// if you run with TestNG, just disable this command
-//@RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"classpath:features"},
         plugin = {"pretty", "json:target/cucumber/cucumber.json", "html:target/cucumber/cucumber.html"},
